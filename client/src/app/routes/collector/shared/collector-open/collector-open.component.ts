3a95807ed3f0cd9ef45d611a7c72a510cf2f58a5
Nis5l <kirchler123@gmail.com>
2025-12-15 18:12:38 +0100

    mobile formatting

--- client/src/app/routes/collector/shared/collector-open/collector-open.component.ts
+++ client/src/app/routes/collector/shared/collector-open/collector-open.component.ts
@@ -106,7 +106,6 @@ export class CollectorOpenComponent extends SubscriptionManagerComponent impleme
 		}
 		let diff: number = (this.packTime.getTime() - Date.now());
 		this.progress =  Math.min(1.0, 1.0 - diff / this.maxTime);
-    console.log("packTime", this.packTime, this.packTime.getTime(), "now", new Date(), Date.now(), "diff", diff, "this.progress", this.progress);
 		setTimeout(() => {
 			this.displayText = this.createDisplayText();
 		});
@@ -126,7 +125,6 @@ export class CollectorOpenComponent extends SubscriptionManagerComponent impleme
 	}
 
 	private createDisplayText(): string{
-    console.log("progress", this.progress, "packTime", this.packTime);
 		if(this.progress >= 1.0) return "Open";
 		if(this.packTime == null) return "Error";
 
